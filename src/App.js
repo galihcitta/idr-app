d1d5736266706179789808ad3a875728158d7b58
galihcitta <galih.citta@gmail.com>
2020-08-04 08:53:18 +0700

    final upload

--- src/App.js
+++ src/App.js
@@ -2,12 +2,28 @@ import React, { useState } from 'react';
 import './App.scss';
 import Header from './components/Header'
 import RupiahInput from './components/RupiahInput'
+import RupiahOutput from './components/RupiahOutput'
 
 function App() {
   const [rupiah, setRupiah] = useState('')
 
   const generateRupiah = (rupiah) => {
     let fracNumber = [100000, 50000, 20000, 10000, 5000, 2000, 1000, 500, 100, 50]
+    let divider = 0, tempMod = 0
+    let rupiahStr = ''
+
+    fracNumber.forEach((item, index, arr) => {
+        divider = parseInt(rupiah/item)
+        if (divider !== 0) {
+            tempMod = rupiah % item
+            rupiah = tempMod
+            rupiahStr = rupiahStr + ` ${divider} x Rp ${item}`
+        }
+        if (index === arr.length - 1) {
+          rupiahStr = rupiahStr + `, Sisa Rp ${rupiah}`
+        }
+    })
+    setRupiah(rupiahStr)
   }
   
 
@@ -15,6 +31,7 @@ function App() {
     <div className="App">
       <Header title="IDR Mini App"  />
       <RupiahInput generateRupiah={generateRupiah} />
+      <RupiahOutput resultRupiah={rupiah}/>
     </div>
   );
 }
---
+++ src/components/RupiahOutput.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function RupiahOutput({ resultRupiah }) {
+    return (
+        <div>
+            {resultRupiah}
+        </div>
+    )
+}
+
+export default RupiahOutput
